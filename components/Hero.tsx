b497f1eee538deda28666e61283f2668b12b09dc
meshJohn <meshjohn1@gmail.com>
2025-02-01 18:57:52 +0200

    update projects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m John, a Frontend Developer based in Egypt.
           </p>
-          <a href="#about">
+          <a href="#projects">
             <MagicButton
               title="Show my work"
               icon={<FaLocationArrow />}
